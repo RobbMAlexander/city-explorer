a96777c6fdd3b8ceddff07236c20dcaa612bc5b1
Robb Alexander <RobbMAlexander@gmail.com>
2022-01-17 23:48:25 -0800

    fixed some broken references, url. still not receiving/rendering API data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import axios from 'axios';
 import CityDisplay from './CityDisplay';
 
 class App extends React.Component {
@@ -12,70 +11,16 @@ class App extends React.Component {
       userSearch: '',
       errMsg: '',
       errRender: false,
-
     }
   }
 
-  handleExplore = async () => {
-
-    try {
-      let cityResults = await axios.get('url')
-
-      this.setState({
-        displayCityData: true,
-        cityData: cityResults.data.results
-      })
-    } catch (error) {
-      this.setState({
-        errRender: true,
-        errMsg: `An error has occurred: ${error.response.status}; ${error.response.data}`
-      })
-    }
-  }
-
-  handleCitySubmit = e => {
-    e.preventDefault();
-    let city = e.target.city.value
-    this.setState({
-      userSearch: e.target.city.value,
-    });
-    this.getCityData(city);
-  }
-
-  getCityData = async (city) => {
-    try {
-      let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.local.REACT_APP_LOCATIONIQ_ACCESS_TOKEN}&q=${city}&format=json`;
-
-      let cityResults = await axios.get(url);
-
-      this.setState({
-        cityData: cityResults.data[0]
-      })
-    } catch (error) {
-      this.setState({
-        errRender: true,
-        errMsg: `An error has occurred: ${error.response.status}; ${error.response.data}`
-
-      })
-    }
-  }
+ 
   render() {
-    let cityRendered = this.state.cityData.map((city, cityIndex) => (
-      <p key={cityIndex}>{city.name}</p>
-    ))
 
     return (
       <>
-        <CityDisplay>
-        </CityDisplay>
+        <CityDisplay />
 
-
-        <form onSubmit={this.handleCitySubmit}>
-          <label>Input A City
-            <input name="city" type="text" />
-          </label>
-          <button type="submit">Explore!</button>
-        </form>
       </>
     );
   }
--- src/CityDisplay.js
+++ src/CityDisplay.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
+import axios from 'axios';
 
 class CityDisplay extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -15,10 +15,54 @@ class CityDisplay extends React.Component {
     }
   }
 
-  render() {
+  handleExplore = async () => {
 
+    try {
+      let cityResults = await axios.get('https://us1.locationiq.com/v1/search.php')
+
+      this.setState({
+        displayCityData: true,
+        cityData: cityResults.data.results
+      })
+    } catch (error) {
+      this.setState({
+        errRender: true,
+        errMsg: `An error has occurred: ${error.response.status}; ${error.response.data}`
+      })
+    }
+  }
+
+  handleCitySubmit = e => {
+    e.preventDefault();
+    let city = e.target.city.value
+    this.setState({
+      userSearch: e.target.city.value,
+    });
+    this.getCityData(city);
+  }
+
+  getCityData = async (city) => {
+    try {
+      let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.local.REACT_APP_LOCATIONIQ_ACCESS_TOKEN}&q=${city}&format=json`;
+
+      let cityResults = await axios.get(url);
+
+      this.setState({
+        cityData: cityResults.data[0]
+      })
+    } catch (error) {
+      this.setState({
+        errRender: true,
+        errMsg: `An error has occurred: ${error.response.status}; ${error.response.data}`
+
+      })
+    }
+  }
+
+  render() {
+    
     let cityRendered = this.state.cityData.map((city, cityIndex) => (
-      <p key={cityIndex}>{city.name}</p>
+      <p key={cityIndex}>{city.name}-- Latitude:{city.lat}, Longitude:{city.lon}</p>
     ))
 
     return (
@@ -30,6 +74,12 @@ class CityDisplay extends React.Component {
           <button onClick={this.handleExplore}>Display City Information</button>
           {this.state.displayCityData ? cityRendered : ''}
           {this.state.renderError && <p>{this.state.errMsg}</p>}
+          <form onSubmit={this.handleCitySubmit}>
+          <label>Input A City
+            <input name="city" type="text" />
+          </label>
+          <button type="submit">Explore!</button>
+        </form>
         </main>
       </>
     );
